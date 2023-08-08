70a334ccee54b717431d042c9065e82c1a636106
eodzhadl0128 <eodzhadl0128@naver.com>
2023-08-08 18:14:05 +0900

    4단계 생성

---
+++ src/banking4/Account.java
@@ -0,0 +1,87 @@
+package banking4;
+
+import java.util.Scanner;
+
+public abstract class Account {
+
+	//멤버변수
+	protected String accountNumber; //계좌번호
+	protected String name; //이름
+	protected int balance; //잔액
+
+	
+	//메소드 호출을 위한 기본 생성자
+	public Account() {}
+
+	//생성자
+	public Account(String accountNumber, String name, int balance) {
+		
+		this.accountNumber = accountNumber;
+		this.name = name;
+		this.balance = balance;
+	}
+
+	//get
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	//set
+	public void setBalance(int balance) {
+		this.balance += balance;
+	}
+	
+	
+	//전체계좌정보출력
+	public abstract void showAccountInfo();
+	
+	//입급처리
+	public abstract boolean plusAccMoney(int money);
+	
+	
+	//출금처리
+	public boolean minusAccMoney(int money) {
+		//잔고가 마이너스 처리되더라도 조건없이 출금처리함
+		balance -= money;
+		return true;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/banking4/NormalAccount.java
@@ -0,0 +1,37 @@
+package banking4;
+
+public class NormalAccount extends Account {
+	
+	//멤버변수
+	private double inter;
+
+	//생성자
+	public NormalAccount(String accountNumber, String name, int balance, double inter) {
+		
+		//부모 생성자 먼저 호출
+		super(accountNumber, name, balance);
+		//멤버변수 초기화
+		this.inter = inter / 100.0;
+	}
+	
+	@Override
+	public boolean plusAccMoney(int money) {
+		
+		balance = (int)(balance + (balance * inter) + money); 
+		
+		return true;
+	}
+	
+	@Override
+	public void showAccountInfo() {
+		
+		System.out.println();
+		System.out.println("******* 계좌 정보 출력 *******");
+		System.out.println("--------------------------");
+		System.out.println("계좌번호 > " + accountNumber);
+		System.out.println("고객 이름 > " + name);
+		System.out.println("잔고 > " + balance);
+		System.out.println("기본이자 > " + (int)(inter * 100) + "%");
+		System.out.println("--------------------------");
+	}
+}
